4191624bfc35420ebd9f846554d8488a4ce387ac
Tz055 <zzhilyaev@Gmail.com>
2025-10-15 18:57:14 +0300

    Initial commit

---
+++ src/client/kotlin/kd/nametags/KdnametagsmodClient.kt
@@ -0,0 +1,9 @@
+package kd.nametags
+
+import net.fabricmc.api.ClientModInitializer
+
+object KdnametagsmodClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
